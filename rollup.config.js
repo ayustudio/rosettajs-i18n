a7167ac8e3408df0a915bf74f13a9e2193d8d452
sandpit.expert <54912605+ayustudio@users.noreply.github.com>
2024-09-01 17:58:48 +0100

    Create rollup.config.js

---
+++ rollup.config.js
@@ -0,0 +1,25 @@
+module.exports = {
+  parser: '@typescript-eslint/parser',
+  extends: [
+    'eslint:recommended',
+    'plugin:@typescript-eslint/recommended',
+    'plugin:prettier/recommended',
+  ],
+  plugins: ['@typescript-eslint'],
+  env: {
+    browser: true,
+    node: true,
+    es6: true,
+  },
+  rules: {
+    '@typescript-eslint/explicit-function-return-type': 'off',
+    '@typescript-eslint/no-explicit-any': 'off',
+    '@typescript-eslint/no-unused-vars': ['error', { argsIgnorePattern: '^_' }],
+    'prettier/prettier': ['error', {}, { usePrettierrc: true }],
+  },
+  settings: {
+    react: {
+      version: 'detect',
+    },
+  },
+};
